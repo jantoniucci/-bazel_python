46436ca0811b75cfa2fb34a47b6dccbfd422890c
javier.antoniucci <jrai@gft.com>
2023-11-25 14:37:38 +0100

    first commit

---
+++ app/main.py
@@ -0,0 +1,17 @@
+from common.calculator import Calculator
+from flask import Flask
+from random import randint
+ 
+app = Flask(__name__)
+calculator = Calculator()
+ 
+@app.route('/')
+def randomNumberCalculator():
+  randomInt1 = randint(0, 250)
+  randomInt2 = randint(0, 250)
+  calculator = Calculator()
+  return "{} + {} = {}?".format(randomInt1, randomInt2, \
+  calculator.add(randomInt1, randomInt2))
+ 
+if __name__ == '__main__':
+  app.run(host='0.0.0.0')
